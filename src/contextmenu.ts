1718d64fda0934a6ba503384afc7bacb4b654864
Mr.Fan <fzm@synron.cn>
2020-07-12 12:05:11 +0800

    fixed type export

--- src/contextmenu.ts
+++ src/contextmenu.ts
@@ -3,14 +3,34 @@
 import './theme/default/index.css';
 
 import {ContextMenu, ContextMenuOptions} from './model/ContextMenu';
+import ContextMenuItem from './model/ContextMenuItem';
 
-const contextMenuGlobal = new ContextMenu();
+class ContextMenuManager {
+    private static readonly contextmenu: ContextMenu = new ContextMenu();
+
+    public static config(globalOptions?: ContextMenuOptions): void {
+        this.contextmenu.config(globalOptions);
+    }
+
+    public static debug(b: boolean): void {
+        this.contextmenu.debug(b);
+    }
+
+    public static hide(): void {
+        this.contextmenu.hide();
+    }
+
+    public static show(menu: Array<ContextMenuItem>, options?: ContextMenuOptions): void {
+        this.contextmenu.show(menu, options);
+    }
+}
+
 if (window) {
     Object.defineProperty(window, 'ContextMenu', {
-        value: contextMenuGlobal,
+        value: ContextMenuManager,
         writable: false,
         configurable: false,
     });
 }
 
-export {contextMenuGlobal as ContextMenu, ContextMenuOptions};
+export {ContextMenuManager as ContextMenu, ContextMenuOptions};
